9510c9f7151c5d60d133c3bfdab0f4be7234f2a2
Akbaroke <akbarmuhammad833@gmail.com>
2022-11-23 15:48:59 +0700

    update ...

--- frontend/src/pages/GantiKode/GantiKode.jsx
+++ frontend/src/pages/GantiKode/GantiKode.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import './GantiKode.css'
 import TopbarBtn from '../../components/TopbarBtn';
 import TopbarPolos from '../../components/TopbarPolos';
 
